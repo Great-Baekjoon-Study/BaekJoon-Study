07ac710dfcc16b325a48da6465f1a360b392c4fe
inhoo <ellyfan@naver.com>
2025-08-05 12:41:26 +0900

    0805 / list1 / 함수설정,if문삭제

--- inhoo/week1/List1_23968_알고리즘 수업 - 버블 정렬 1.py
+++ inhoo/week1/List1_23968_알고리즘 수업 - 버블 정렬 1.py
@@ -1,19 +1,26 @@
-count = 0
-
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-for i in range(N-1, 0, -1):
-    for j in range(0,i):   
-        if A[j] > A[j+1]:
-            A[j], A[j+1] = A[j+1], A[j]
-            count+=1
+count = 0
 
-            if count == K:
+def bubble_sort():
+    global count
+    for i in range(N-1, 0, -1):
+        for j in range(0,i):   
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+                count+=1
 
-                if A[j]< A[j+1]:
-                    print(A[j],A[j+1])
-                    exit()
+                if count == K:
+                    return f'{A[j]} {A[j+1]}'
+                    
                                          
-if count < K:
-    print(-1)
+    if count < K:
+        return -1
+
+
+print(bubble_sort())
+
+#함수 안에서 변수를 읽기만 할 때는 global 안 써도 됨
+#다만 변수에 값을 대입할 때는 그 변수를 local 취급함.
+#return print() 하면 print()의 반환값 None이 반환된다.
