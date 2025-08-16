f2160a69d4320a1d38e3effb611eb27f59d741fb
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2025-08-16 09:19:09 +0900

    0816/델타/미로탐색수정

--- inhoo/week1/List2_2178_미로탐색.py
+++ inhoo/week1/List2_2178_미로탐색.py
@@ -1,48 +1,33 @@
-#미로탐색
-
-# 뒤 :뒤에서 온 길은 다시는 가지 말기(다른 길 실패했을 경우에는 다시 돌아가기)
-# 왼,앞,오
-# 범위 밖으로 벗어나는지 확인
-# 3갈래 길이 발생했을 때, 3개 다 가보고 판단
-#가서 갈 곳이 없을 때, 다시 조건문으로 돌아가기
+# DFS 사용, 런타임 에러 발생
+# 갈림길의 좌표, 최소 칸의 수
 
 
-N, M = map(int, input().split())
 
-maze = [list(map(int, input().strip())) for _ in range(N)]
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+visited[0][0] = 1
+# 1 2 0
+# 0 0 0
+# 0 0 0
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+cnt = 0
 
+def dfs(x,y): #x,y는 시작점 # 좌표
+    
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+    
+        if 0<=nx<n and 0<=ny<m and graph[nx][ny]==1:
+            if visited[nx][ny] == 0 or visited[nx][ny] > visited[x][y] + 1:
+                #한 번도 안 가본 길이거나 더 짧은 거리라면 거리값을 갱신
+                visited[nx][ny] = visited[x][y] + 1
+                if nx == n-1 and ny ==m-1:
+                    return
+                dfs(nx,ny)
+    return
 
-distance = [[0] * M for _ in range(N)]
+dfs(0,0)
+print(visited[n-1][m-1])
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-
-start_point = [(0, 0)]  # 시작점 넣기
-front = 0         # 맨 앞에서부터 차례대로 꺼내 쓰기 위한 번호(인덱스)
-
-# 시작칸도 "칸 수"에 포함되니까 1로 시작
-distance[0][0] = 1
-
-# 6 이제 반복 시작
-while front < len(start_point):
-    # 앞에서 하나 꺼내오기
-    i, j = start_point[front]
-    front += 1
-
-    #도착지에 도착
-    if i == N - 1 and j == M - 1:
-        print(distance[i][j])
-        break
-
-    # 현재칸에서 다음 행
-    for d in range(4):
-        nx = i + dx[d]
-        ny = j + dy[d]
-
-        #판 안에서만 돌리기
-        if 0 <= nx < N and 0 <= ny < M:
-            if maze[nx][ny] == 1 and distance[nx][ny] == 0:
-                distance[nx][ny] = distance[i][j] + 1
-
-                start_point.append((nx, ny))
