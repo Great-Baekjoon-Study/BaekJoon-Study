2a638d8dd55578aec2e7be1d6418b418fc1c50a9
Kim Sehyeon <sshyeoniz@naver.com>
2025-09-11 21:33:03 +0900

    0911_week4_제출

--- sehyeon/week3/A형_면접.py
+++ sehyeon/week3/A형_면접.py
@@ -3,7 +3,8 @@ T = int(input())
 
 def cal_score(N, K, wrong_idx):
     # 튜플 -> 집합으로 바꿔 시간복잡도 O(1)로 바꿔줌
-    wrong = wrong_idx
+    wrong = set(wrong_idx)
+    # 점수, 카운터
     score, cnt = 0, 0
 
     for i in range(N):
@@ -12,10 +13,15 @@ def cal_score(N, K, wrong_idx):
             cnt = 0
         # 정답이면
         else:
+            # 카운터 +1 하는데
             cnt += 1
+            # 연속으로 맞춘 문제가 K보다 작으면
             if cnt < K:
+                # 점수 +1
                 score += 1
-            else:   # cnt == K
+            # cnt == K
+            else:
+                # 점수 +1 후 2배, 카운터는 0   
                 score += 1
                 score *= 2
                 cnt = 0
@@ -26,6 +32,7 @@ def cal_score(N, K, wrong_idx):
 for test_case in range(1, T+1):
     N, M, K = map(int, input().split())
 
+    # 최소값을 무한대로 가정
     min_score = float('inf')
     for wrong_idx in combinations(range(N), N-M):
         min_v = cal_score(N, K, wrong_idx) 
@@ -34,6 +41,14 @@ for test_case in range(1, T+1):
             min_score = min_v
     
     print(f'#{test_case} {min_score}')
+
+
+
+
+
+
+
+
 
 
 '''
